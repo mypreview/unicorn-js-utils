331d172b06b66658d08b0e0892a94b932b2c7d3a
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-05-02 15:06:07 +0300

    Convert hex unicode to Emoji

---
+++ src/hexToEmoji/index.js
@@ -0,0 +1,28 @@
+/**
+ * Utility for libraries from the `Lodash`.
+ *
+ * @ignore
+ */
+import { join, split, toLower } from 'lodash';
+
+/**
+ * Gets a formatted version of the post content provided.
+ *
+ * @ignore
+ */
+import formattedContent from './../formattedContent';
+
+/**
+ * Converts a given hex-unicode into an Emoji icon.
+ *
+ * @function
+ * @since       1.0.0
+ * @param  	    {string}    value    Emoji specific hex code.
+ * @return 	    {any}                Rendered Emoji.
+ * @example
+ *
+ * hexToEmoji( '1F603' );
+ *
+ * // => string 😃
+ */
+export default ( value ) => formattedContent( `&#x${ join( split( toLower( value ), ' ' ), '&#x' ) };` );
