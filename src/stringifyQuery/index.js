120b5199813452e71737b83def44b638a70f8fde
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-05-02 15:01:13 +0300

    Stringify an array of objects into a query string

---
+++ src/stringifyQuery/index.js
@@ -0,0 +1,25 @@
+/**
+ * Utility for libraries from the `Lodash`.
+ *
+ * @ignore
+ */
+import { join, keys, map, values } from 'lodash';
+
+/**
+ * Stringify an array of objects into a query string.
+ *
+ * @function
+ * @since       1.0.0
+ * @param  	    {Array}     input    The array of objects to query stringify.
+ * @return 	    {string}             Returns a stringified query.
+ * @example
+ *
+ * stringifyQuery( [ { product: '1885' }, { action: 'hook_name' } ] );
+ *
+ * // => string 'product=1885&action=hook_name'
+ */
+export default ( input ) =>
+	join(
+		map( input, ( item ) => `${ keys( item ) }=${ values( item ) }` ),
+		'&'
+	);
