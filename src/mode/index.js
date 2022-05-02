8cf0261de53bda33af6a71e3c1b2f34a34a94ace
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-05-02 15:06:26 +0300

    Determines which element has the highest occurrence

---
+++ src/mode/index.js
@@ -0,0 +1,21 @@
+/**
+ * Utility for libraries from the `Lodash`.
+ *
+ * @ignore
+ */
+import { filter, last, sortBy } from 'lodash';
+
+/**
+ * Determines which element has the highest occurrence (mode) in the given array.
+ *
+ * @function
+ * @since       1.0.0
+ * @param  	    {Array}     input    The array to process.
+ * @return 	    {string}             Mode element.
+ * @example
+ *
+ * mode( ['pear', 'apple', 'orange', 'apple'] );
+ *
+ * // => string "apple"
+ */
+export default ( input ) => last( sortBy( input, ( a, b ) => filter( input, ( v ) => v === a ).length - filter( input, ( v ) => v === b ).length ) );
