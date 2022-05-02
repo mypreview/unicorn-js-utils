353f6d5ce0c5290e984245d492110ce2ff7d6825
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-05-02 15:01:21 +0300

    Slugifies every string

---
+++ src/slugify/index.js
@@ -0,0 +1,34 @@
+/**
+ * Slugifies every string, even when it contains unicode!
+ *
+ * @ignore
+ */
+import slug from 'slugify';
+
+/**
+ * An implementation of PHP's `strip_tags` in Node.js.
+ *
+ * @ignore
+ */
+import striptags from 'striptags';
+
+/**
+ * Slugifies every string, even when it contains unicode!
+ * Properly strips all HTML tags including script and style.
+ *
+ * @function
+ * @since       1.0.0
+ * @param  	    {string}    input    The value to slugify.
+ * @return 	    {string}             Converted value to slug.
+ * @example
+ *
+ * slugify( 'unicode is ♥' );
+ *
+ * // => string 'unicode-is-love'
+ */
+export default ( input ) =>
+	slug( striptags( input ), {
+		replacement: '-',
+		remove: /[*_+~()'"!?\/\-—–−:@^|&#.,;%<>{}]/g,
+		lower: true,
+	} );
