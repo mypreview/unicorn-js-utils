85197ade3460a0cca46555c50fd8c606d6ec4c00
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-05-02 12:58:16 +0300

    Converts empty spaces to HTML character entity

---
+++ src/spaceToNbsp/index.js
@@ -0,0 +1,24 @@
+/**
+ * Utility for libraries from the `Lodash`.
+ *
+ * @ignore
+ */
+import { join, split } from 'lodash';
+
+/**
+ * Converts empty spaces to its corresponsding HTML character entity (&nbsp;).
+ * A non-breaking space is a space that will not break into a new line.
+ * Two words separated by a non-breaking space will stick together (not break into a new line).
+ * This method is handy when breaking the words might be disruptive.
+ *
+ * @function
+ * @since       1.0.0
+ * @param  	    {string}    input    String to convert.
+ * @return 	    {string}             Converted value.
+ * @example
+ *
+ * spaceToNbsp( 'Hello World! This is an example.' );
+ *
+ * // => string 'Hello&nbsp;World!&nbsp;This&nbsp;is&nbsp;an&nbsp;example.'
+ */
+export default ( input ) => join( split( input, /\s/g ), '&nbsp;' );
