c25cad89b207d0be5899df749696e5945f00e5df
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-05-02 12:56:30 +0300

    Extract selected block style

---
+++ src/blockStyleSlug/index.js
@@ -0,0 +1,21 @@
+/**
+ * Utility for libraries from the `Lodash`.
+ *
+ * @ignore
+ */
+import { invoke, nth, replace } from 'lodash';
+
+/**
+ * Extracts selected block style slug from the provided block specific CSS class names.
+ *
+ * @function
+ * @since       1.0.0
+ * @param       {string}    input    CSS class names assigned to the block.
+ * @return      {string}             Style name associated or selected for this block.
+ * @example
+ *
+ * blockStyleSlug( 'wp-block-prefix-spacer is-style-fancy test-class-name' );
+ *
+ * // => string 'fancy'
+ */
+export default ( input ) => replace( nth( invoke( input, 'match', /is-style-[^{\s]*/ ) ), /is-style-/, '' );
