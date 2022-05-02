0ce36cec57c929efa4f1f62eaa7f8da9097763e7
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-05-02 12:58:01 +0300

    Remove an item from the array at given index

---
+++ src/removeAtIndex/index.js
@@ -0,0 +1,22 @@
+/**
+ * Utility for libraries from the `Lodash`.
+ *
+ * @ignore
+ */
+import { add, concat, slice } from 'lodash';
+
+/**
+ * Removes an item from the array at a specific index.
+ *
+ * @function
+ * @since       1.0.0
+ * @param  	    {Array}     input    The array to remove from.
+ * @param  	    {number}    index    Given array index to remove the value from.
+ * @return 	    {Array}              Returns the new updated array.
+ * @example
+ *
+ * removeAtIndex( [ 'a', 'd', 'b' ], 1 );
+ *
+ * // => Array [ 'a', 'b' ]
+ */
+export default ( input, index ) => concat( slice( input, 0, index ), slice( input, add( index, 1 ) ) );
