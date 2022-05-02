ccd2c2862c8616e63ab01033254feb7b03ccd54f
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-05-02 15:05:38 +0300

    Filter object elements according to given predicate function

---
+++ src/filterCollectionByValues/index.js
@@ -0,0 +1,49 @@
+/**
+ * Utility functions from `Lodash`.
+ *
+ * @ignore
+ */
+import { forEach, find, matchesProperty } from 'lodash';
+
+/**
+ * Creates a function that performs a partial deep comparison between the `id` of
+ * a given object to the passed value. Returns true if the object value is equivalent,
+ * otherwise false.
+ *
+ * This function is used as the default predicate for `filterCollectionByValues`.
+ *
+ * @param     {any}         value    A value to check for.
+ * @return    {Function}             Predicate comparison function.
+ */
+const matchId = ( value ) => matchesProperty( 'id', value );
+
+/**
+ * Iterates over elements of `input` and returns all entries from `collection`
+ * for which `predicate` returns truthy.
+ *
+ * Notice that this function preserves the order of `input` in the result set
+ * and is thus most useful if a collection must be filtered in an order that is
+ * specified by another collection.
+ *
+ * @function
+ * @since       1.0.0
+ * @param       {Array}       input         The input to search for.
+ * @param       {Array}       collection    The collection to search elements in.
+ * @param       {Function}    predicate     The predicate function to be applied in every iteration.
+ * @return      {Array}                     All matching entries in collection.
+ * @example
+ *
+ * filterCollectionByValues( [ 1, 3 ], [ { id: 1, content: {} }, { id: 2, content: {} }, { id: 3, content: {} } ] );
+ *
+ * // => Array [ { id: 1, content: {} }, { id: 3, content: {} } ]
+ */
+export default ( input, collection, predicate = matchId ) => {
+	const existing = [];
+	forEach( input, ( value ) => {
+		const match = find( collection, predicate( value ) );
+		if ( Boolean( match ) ) {
+			existing.push( match );
+		}
+	} );
+	return existing;
+};
