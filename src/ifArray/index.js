af570863b002c51585f52dfe425e3dedafeb2da3
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-05-02 12:57:29 +0300

    Check if given value is not an empty array

---
+++ src/ifArray/index.js
@@ -0,0 +1,21 @@
+/**
+ * Utility for libraries from the `Lodash`.
+ *
+ * @ignore
+ */
+import isArray from 'lodash/isArray';
+
+/**
+ * Check if value is classified as an array object and is not empty.
+ *
+ * @function
+ * @since       1.0.0
+ * @param  	    {Array}      input    The value to check.
+ * @return 	    {boolean}             Whether the argument provided is a non-empty array.
+ * @example
+ *
+ * ifArray( [] );
+ *
+ * // => boolean false
+ */
+export default ( input ) => isArray( input ) && !! input.length;
