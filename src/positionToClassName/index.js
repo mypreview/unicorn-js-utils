c6bd885d12012384cf48c1fa8a83a151ff724f62
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-05-02 15:02:12 +0300

    Generate content-position CSS class name

---
+++ src/positionToClassName/index.js
@@ -0,0 +1,35 @@
+/**
+ * Utility for libraries from the `Lodash`.
+ *
+ * @ignore
+ */
+import get from 'lodash/get';
+
+/**
+ * Determine whether the content is center positioned.
+ *
+ * @ignore
+ */
+import isPositionCenter from '../isPositionCenter';
+
+/**
+ * Possible background position inputs.
+ *
+ * @ignore
+ */
+import positions from './positions';
+
+/**
+ * Generates content-position CSS class name based on provided input.
+ *
+ * @function
+ * @since       1.0.0
+ * @param  	    {string}    input    CSS position input.
+ * @return 	    {string}             Corresponding CSS class name based on the provided position.
+ * @example
+ *
+ * positionToClassName( 'center right' );
+ *
+ * // => string 'is-position-center-right'
+ */
+export default ( input ) => ( isPositionCenter( input ) ? '' : get( positions, input ) );
