36f59babe402f0d7c4c22d1ea7f31cae198cd305
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-05-02 15:06:17 +0300

    Determine whether the content is center positioned

---
+++ src/isPositionCenter/index.js
@@ -0,0 +1,21 @@
+/**
+ * Utility for libraries from the `Lodash`.
+ *
+ * @ignore
+ */
+import isEqual from 'lodash/isEqual';
+
+/**
+ * Determine whether the content is center positioned.
+ *
+ * @function
+ * @since       1.0.0
+ * @param  	    {string}     input    The value to check.
+ * @return 	    {boolean}             Return true if content is center positioned.
+ * @example
+ *
+ * isPositionCenter( 'center' );
+ *
+ * // => boolean true
+ */
+export default ( input ) => isEqual( input, 'center center' ) || isEqual( input, 'center' );
