325c1f90592c872da2351f889893606baa19e761
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-05-02 15:04:52 +0300

    Creates an object composed of the picked object props

---
+++ src/deepPick/index.js
@@ -0,0 +1,26 @@
+/**
+ * Utility for libraries from the `Lodash`.
+ *
+ * @ignore
+ */
+import { first, forEach, get, set, split } from 'lodash';
+
+/**
+ * Creates an object composed of the (nested) picked object properties.
+ *
+ * @function
+ * @since       1.0.0
+ * @param  	    {Object}    collection    The source object.
+ * @param  	    {Array}     paths         The property paths to pick.
+ * @return      {Object}                  Returns the new object.
+ * @example
+ *
+ * deepPick( { id: 1, title: { rendered: 'Hello world!' } }, [ 'title.rendered' ] );
+ *
+ * // => Object { title: 'Hello world!' }
+ */
+export default ( collection, paths ) => {
+	const picked = {};
+	forEach( paths, ( path ) => set( picked, first( split( path, '.' ) ), get( collection, path ) ) );
+	return picked;
+};
