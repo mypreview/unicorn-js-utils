b45ed7f064d7a84275d17eac61fbd6f0f178e994
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-05-02 13:05:13 +0300

    Generate shortcode tag

---
+++ src/shortcode/index.js
@@ -0,0 +1,30 @@
+/**
+ * Utility for libraries from the `Lodash`.
+ *
+ * @ignore
+ */
+import { forOwn, snakeCase, toLower } from 'lodash';
+
+/**
+ * Generate shortcode.
+ *
+ * @function
+ * @since       1.0.0
+ * @param       {string}    tagName       Shortcode tag name.
+ * @param       {Object}    attributes    Shortcode attributes.
+ * @return      {string}                  Generated shortcode.
+ * @example
+ *
+ * shortcode( 'prefix_teams', { posts: [1, 2], number: 2 } );
+ *
+ * // => string '[prefix_teams posts="1,2" number="2"]'
+ */
+export default ( tagName, attributes ) => {
+	let tag = `[${ snakeCase( tagName ) }`;
+	forOwn( attributes, ( value, key ) => {
+		tag += ` ${ toLower( snakeCase( key ) ) }="${ value }"`;
+	} );
+	tag += ']';
+
+	return tag;
+};
